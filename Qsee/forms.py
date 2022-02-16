85dd04a2a099c8acd9e5169c34f8a0f46550394f
bbunce <b.bunce@nhs.net>
2022-02-16 15:55:00 +0000

    add form date widgets

--- Qsee/forms.py
+++ Qsee/forms.py
@@ -2,9 +2,12 @@ from django import forms
 
 class TestInputForm(forms.Form):
     """Control and Analyser ID returned from test_input in view.py"""
+    # Set user min and max has this is the number of cycles the PCR runs for
     result = forms.FloatField(min_value=0.0, max_value=45.0)
-    test_date = forms.DateTimeField(required=False)
+    # add a widget to ensure that the date is entered correctly
+    test_date = forms.DateTimeField(widget=forms.SelectDateWidget())
     operator = forms.CharField()
+    # note field is not required
     note = forms.CharField(required=False)
 
 class AssayForm(forms.Form):
@@ -14,7 +17,9 @@ class ControlForm(forms.Form):
     assay_id = forms.IntegerField()
     control_name = forms.CharField()
     lot_number = forms.CharField()
-    date_added = forms.DateField()
+    # add a widget to ensure that the date is entered correctly
+    date_added = forms.DateField(widget=forms.SelectDateWidget())
+    # not required as control maybe received but not used yet
     active = forms.BooleanField(required=False)
 
 class AnalyserForm(forms.Form):
