c67e40e2e1854f9137039b4e76d4c25dd875b360
George Doyle <george.doyle@nbt.nhs.uk>
2022-02-17 12:36:13 +0000

    Spelling

--- Qsee/templates/Qsee/test_frontend.py
+++ Qsee/templates/Qsee/test_frontend.py
@@ -8,14 +8,13 @@ class Testindex(unittest.TestCase):
         """set up function to set the web driver, in this case chrome"""
         self.driver=webdriver.Chrome('./chromedriver')
     def test_input_form (self):
-
-        """Please ensure server is running on local server before beginning test and
-        that you have install the correct chromedriver version for your system..."""
+        """Please ensure server is running locally before beginning test and
+        that you have installed the correct chromedriver version for your system"""
 
         driver = self.driver
-        driver.get('http://127.0.0.1:8000/test_input/8/3/')
+        driver.get('http://127.0.0.1:8000/test_input/8/3/') # Test control
 
-        driver.find_element(By.ID, 'id_result').send_keys(random.randint(27, 30))
+        driver.find_element(By.ID, 'id_result').send_keys(random.randint(27, 30))  # Random int within acceptable range
         driver.find_element(By.ID, 'id_test_date').send_keys('20/01/2023')
         driver.find_element(By.ID, 'id_operator').send_keys('GD')
         driver.find_element(By.ID, 'id_note').send_keys(random.randint(1, 30000))
